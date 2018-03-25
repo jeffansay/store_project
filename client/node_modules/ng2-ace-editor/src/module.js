c88edeade6efd9dfa427a1e2d69b2a9173d3d612
jeffansay <jeffansay21@gmail.com>
2018-03-25 18:19:02 +0800

    done update npm install in the client-side

---
+++ client/node_modules/ng2-ace-editor/src/module.js
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { AceEditorComponent } from "./component";
+import { AceEditorDirective } from "./directive";
+var list = [
+    AceEditorComponent,
+    AceEditorDirective
+];
+var AceEditorModule = (function () {
+    function AceEditorModule() {
+    }
+    AceEditorModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: list.slice(),
+                    imports: [],
+                    providers: [],
+                    exports: list
+                },] },
+    ];
+    /** @nocollapse */
+    AceEditorModule.ctorParameters = function () { return []; };
+    return AceEditorModule;
+}());
+export { AceEditorModule };
+//# sourceMappingURL=module.js.map
